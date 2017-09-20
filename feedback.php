f51820a681d7204572379d5a0029552d117e8b5f
uktagore <uktagore@gmail.com>
2017-09-20 20:53:44 +0530

    commit special

--- feedback.php
+++ feedback.php
@@ -2,9 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<title>NETCAMP || TEST YOUR SKILL </title>
+<title>NETCAMP || FEEDBACK </title>
 <link  rel="stylesheet" href="css/bootstrap.min.css"/>
  <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
  <link rel="stylesheet" href="css/main.css">
@@ -13,25 +11,36 @@
 
   <script src="js/bootstrap.min.js"  type="text/javascript"></script>
  	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+	<!--alert message-->
 <?php if(@$_GET['w'])
 {echo'<script>alert("'.@$_GET['w'].'");</script>';}
 ?>
-<script>
-function validateForm() {var y = document.forms["form"]["name"].value;	var letters = /^[A-Za-z]+$/;if (y == null || y == "") {alert("Name must be filled out.");return false;}var z =document.forms["form"]["college"].value;if (z == null || z == "") {alert("college must be filled out.");return false;}var x = document.forms["form"]["email"].value;var atpos = x.indexOf("@");
-var dotpos = x.lastIndexOf(".");if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {alert("Not a valid e-mail address.");return false;}var a = document.forms["form"]["password"].value;if(a == null || a == ""){alert("Password must be filled out");return false;}if(a.length<5 || a.length>25){alert("Passwords must be 5 to 25 characters long.");return false;}
-var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords must match.");return false;}}
-</script>
-
+<!--alert message end-->
 
 </head>
 
 <body>
-<div class="header">
-<div class="row">
+
+<!--header start-->
+<div class="row header">
 <div class="col-lg-6">
 <span class="logo">Test Your Skill</span></div>
-<div class="col-md-2 col-md-offset-4">
-<a href="#" class="pull-right btn sub1" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Signin</b></span></a></div>
+<div class="col-md-2">
+</div>
+<div class="col-md-4">
+<?php
+ include_once 'dbConnection.php';
+session_start();
+  if((!isset($_SESSION['email']))){
+echo '<a href="#" class="pull-right sub1 btn title3" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;Signin</a>&nbsp;';}
+else
+{
+echo '<a href="logout.php?q=feedback.php" class="pull-right sub1 btn title3"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Signout</a>&nbsp;';}
+?>
+
+<a href="index.php" class="pull-right btn sub1 title3"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home</a>&nbsp;
+</div></div>
+
 <!--sign in modal start-->
 <div class="modal fade" id="myModal">
   <div class="modal-dialog">
@@ -54,7 +63,6 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
   </div>
 </div>
 
-
 <!-- Password input-->
 <div class="form-group">
   <label class="col-md-3 control-label" for="password"></label>
@@ -76,99 +84,59 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 </div><!-- /.modal -->
 <!--sign in modal closed-->
 
-</div><!--header row closed-->
-</div>
+<!--header end-->
 
 <div class="bg1">
 <div class="row">
-
-<div class="col-md-7"></div>
-<div class="col-md-4 panel">
-<!-- sign in form begins -->  
-  <form class="form-horizontal" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
-<fieldset>
-
-
+<div class="col-md-3"></div>
+<div class="col-md-6 panel" style="background-image:url(image/bg1.jpg); min-height:430px;">
+<h2 align="center" style="font-family:'typo'; color:#000066">FEEDBACK/REPORT A PROBLEM</h2>
+<div style="font-size:14px">
+<?php if(@$_GET['q'])echo '<span style="font-size:18px;"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span>&nbsp;'.@$_GET['q'].'</span>';
+else
+{echo' 
+You can send us your feedback through e-mail on the following e-mail id:<br />
+<div class="row">
+<div class="col-md-1"></div>
+<div class="col-md-10">
+<a href="mailto:chiraggoel.53784@gmail.com" style="color:#000000">sunnygkp10@gmail.com</a><br /><br />
+</div><div class="col-md-1"></div></div>
+<p>Or you can directly submit your feedback by filling the enteries below:-</p>
+<form role="form"  method="post" action="feed.php?q=feedback.php">
+<div class="row">
+<div class="col-md-3"><b>Name:</b><br /><br /><br /><b>Subject:</b></div>
+<div class="col-md-9">
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-12 control-label" for="name"></label>  
-  <div class="col-md-12">
-  <input id="name" name="name" placeholder="Enter your name" class="form-control input-md" type="text">
-    
-  </div>
-</div>
+  <input id="name" name="name" placeholder="Enter your name" class="form-control input-md" type="text"><br />    
+   <input id="name" name="subject" placeholder="Enter subject" class="form-control input-md" type="text">    
 
+</div>
+</div>
+</div><!--End of row-->
+
+<div class="row">
+<div class="col-md-3"><b>E-Mail address:</b></div>
+<div class="col-md-9">
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-12 control-label" for="gender"></label>
-  <div class="col-md-12">
-    <select id="gender" name="gender" placeholder="Enter your gender" class="form-control input-md" >
-   <option value="Male">Select Gender</option>
-  <option value="M">Male</option>
-  <option value="F">Female</option> </select>
-  </div>
+  <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">    
+ </div>
 </div>
+</div><!--End of row-->
 
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-12 control-label" for="name"></label>  
-  <div class="col-md-12">
-  <input id="college" name="college" placeholder="Enter your college name" class="form-control input-md" type="text">
-    
-  </div>
+<div class="form-group"> 
+<textarea rows="5" cols="8" name="feedback" class="form-control" placeholder="Write feedback here..."></textarea>
 </div>
-
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-12 control-label title1" for="email"></label>
-  <div class="col-md-12">
-    <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
-    
-  </div>
+<div class="form-group" align="center">
+<input type="submit" name="submit" value="Submit" class="btn btn-primary" />
 </div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-12 control-label" for="mob"></label>  
-  <div class="col-md-12">
-  <input id="mob" name="mob" placeholder="Enter your mobile number" class="form-control input-md" type="number">
-    
-  </div>
-</div>
-
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-12 control-label" for="password"></label>
-  <div class="col-md-12">
-    <input id="password" name="password" placeholder="Enter your password" class="form-control input-md" type="password">
-    
-  </div>
-</div>
-
-<div class="form-group">
-  <label class="col-md-12control-label" for="cpassword"></label>
-  <div class="col-md-12">
-    <input id="cpassword" name="cpassword" placeholder="Conform Password" class="form-control input-md" type="password">
-    
-  </div>
-</div>
-<?php if(@$_GET['q7'])
-{ echo'<p style="color:red;font-size:15px;">'.@$_GET['q7'];}?>
-<!-- Button -->
-<div class="form-group">
-  <label class="col-md-12 control-label" for=""></label>
-  <div class="col-md-12"> 
-    <input  type="submit" class="sub" value="sign up" class="btn btn-primary"/>
-  </div>
-</div>
-
-</fieldset>
-</form>
+</form>';}?>
 </div><!--col-md-6 end-->
+<div class="col-md-3"></div></div>
 </div></div>
 </div><!--container end-->
+
 
 <!--Footer start-->
 <div class="row footer">
@@ -179,6 +147,7 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 <a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
 <div class="col-md-3 box">
 <a href="#" data-toggle="modal" data-target="#developers">Developers</a>
+
 </div>
 <div class="col-md-3 box">
 <a href="feedback.php" target="_blank">Feedback</a></div></div>
